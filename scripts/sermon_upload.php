765613ae10a2b52e440d85c0b63a290c5d30445e
Travis Wooley <antgiant@gmail.com>
2013-02-24 12:13:44 -0500

    File must not exist. Also, some minor refactoring to make the change easier.

--- scripts/sermon_upload.php
+++ scripts/sermon_upload.php
@@ -5,16 +5,18 @@
 </p>
 </form>
 <?php
+$path = "/home/wintersp/public_html/site/1/scripts/";
 foreach ($_FILES["file"]["error"] as $key => $error) {
     if ($error == UPLOAD_ERR_OK 
         //Must be an MP3 File
         && $_FILES["file"]["type"][$key] == "audio/mp3"
         //Must be 100KB or larger
-        && $_FILES["file"]["size"][$key] > 102400) {
+        && $_FILES["file"]["size"][$key] > 102400
+        //File must not exist
+        && !file_exists($path.preg_replace("/[^a-z0-9 \[\]{}.,()&`=\-]/i", "_", $_FILES["file"]["name"][$key]))) {
         $tmp_name = $_FILES["file"]["tmp_name"][$key];
-        $name = $_FILES["file"]["name"][$key];
-        $name = preg_replace("/[^a-z0-9 \[\]{}.,()&`=\-]/i", "_", $name);
-        move_uploaded_file($tmp_name, "/home/wintersp/public_html/site/1/scripts/$name");
+        $name = preg_replace("/[^a-z0-9 \[\]{}.,()&`=\-]/i", "_", $_FILES["file"]["name"][$key]);
+        move_uploaded_file($tmp_name, $path.$name);
     }
 }
 ?>
