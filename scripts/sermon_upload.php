ac18ead63cd6e5fc7edd012f51f1b328b238b335
Travis Wooley <antgiant@gmail.com>
2013-02-24 11:05:58 -0500

    Super Simple Super Dangerous File Upload System

--- scripts/sermon_upload.php
+++ scripts/sermon_upload.php
@@ -1,3 +1,15 @@
+<form action="" method="post" enctype="multipart/form-data">
+<p>File:
+<input type="file" name="file[]" />
+<input type="submit" value="Send" />
+</p>
+</form>
 <?php
-phpinfo();
+foreach ($_FILES["file"]["error"] as $key => $error) {
+    if ($error == UPLOAD_ERR_OK) {
+        $tmp_name = $_FILES["file"]["tmp_name"][$key];
+        $name = $_FILES["file"]["name"][$key];
+        move_uploaded_file($tmp_name, "/home/wintersp/public_html/site/1/scripts/$name");
+    }
+}
 ?>
