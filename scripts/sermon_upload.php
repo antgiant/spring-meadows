94dc557483a7451a04a6b2d49d47b4c39024af86
Travis Wooley <antgiant@gmail.com>
2013-02-24 12:00:32 -0500

    File must be larger than 100KB

--- scripts/sermon_upload.php
+++ scripts/sermon_upload.php
@@ -6,7 +6,11 @@
 </form>
 <?php
 foreach ($_FILES["file"]["error"] as $key => $error) {
-    if ($error == UPLOAD_ERR_OK && $_FILES["file"]["type"][$key] == "audio/mp3") {
+    if ($error == UPLOAD_ERR_OK 
+        //Must be an MP3 File
+        && $_FILES["file"]["type"][$key] == "audio/mp3"
+        //Must be 100KB or larger
+        && $_FILES["file"]["size"][$key] > 102400) {
         $tmp_name = $_FILES["file"]["tmp_name"][$key];
         $name = $_FILES["file"]["name"][$key];
         $name = preg_replace("/[^a-z0-9 \[\]{}.,()&`=\-]/i", "_", $name);
