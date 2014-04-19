fc8ef0168043462ef17bcbe98ae47b4bd6c367b0
Travis Wooley <antgiant@gmail.com>
2014-04-19 12:34:02 -0400

    Bringing in Rotating Images from template

--- home.php
+++ home.php
@@ -1,53 +1,22 @@
-<div style="text-align: center;">
-	<div class="third" style="margin-right: 0px; border-right-width: 0px;">
-		<h3>
-			Welcome&nbsp;</h3>
-		<p>
-			I am delighted that you are visiting our website. We want to share our love for Jesus with you and provide meaningful resources that will support your spiritual growth. We offer vibrant nurturing programs for seniors, couples, singles, youth, and children which include engaging activities during the week.</p>
-		<p>
-			We would love for you to visit us in our new facility and be touched by the Holy Spirit through our worship services which are:</p>
-		<p style="text-align: left;">
-			<strong>Saturdays</strong>:<br />
-			<em>9:30 am</em> Bible study for adults, youth, and children<br />
-			<em>10:45 am</em> Worship<br />
-			<br />
-			A live stream of the worship service is available on the top of this page every Sabbath (Saturday) morning starting at 10:45 am.</p>
-		<p>
-			I look forward to meeting each of you,</p>
-		<p>
-			With Christian love,</p>
-		<p>
-			Pastor Frank</p>
-		<h3 id="directions">
-			<?php
-                    $ver = array();
-                    $dest = urlencode('5783 N Ronald Reagan Blvd. Sanford, FL 32773');
-                    preg_match("/.*CPU [^ ]*( ){0,1}OS ([0-9]+)_([0-9]+) like Mac OS.*/", $_SERVER['HTTP_USER_AGENT'], $ver);
-                    //Check for IOS Version 6 or above
-                    //If you are editing this be sure to edit the template as well.
-                    if(isset($ver[2]) && $ver[2] >= 6) {
-                      Echo '			<a href="http://maps.apple.com/maps?daddr='.$dest.'">Click for Directions</a>';
-                    }
-                    else {
-                      Echo '			<a href="http://maps.google.com/maps?saddr=&amp;daddr='.$dest.'">Click for Directions</a>';
-                    }
-                  ?></h3>
-	</div>
-	<div class="third" style="margin-right: 0px; border-right-width: 0px;">
-		<h3>
-			Thought For Today</h3>
-		##rssdigest14##
-		<h3>
-			Latest Message</h3>
-		<?php
-If (mysql_ping()) {
-  $temp = mysql_query("select title, speaker, file_date, link from podcast where zone = 1 and Status = 'active'  Order by file_date desc  LIMIT 1");
-  while (($tr = mysql_fetch_assoc($temp)) != false) {
-    $podcast[] = $tr;
+<?php
+  $temp = mysql_query("select * from calendar_details where active = 1 and CategoryID = 13 and DisplayStart <= '".date("Y-m-d")."' and DisplayStop >= '".date("Y-m-d")."' and StartTime <= '".date("H:i:s")."' and StopTime >= '".date("H:i:s")."'");
+  While($tr = mysql_fetch_assoc($temp)) {
+        $DB_Data[] = $tr;
   }
+        $no_live_stream = array(
+                                 '10-29-2011' => "We apologize but today's live stream is not possible due to a power outage.",
+                                 '11-12-2011' => "We apologize but today's live stream is not available due to a church retreat.",
+//                                 '9-29-2012' => "We apologize but we are currently having technical problems with today's live stream.",
+                                 '10-13-2012' => "We apologize but today's live stream is not available due to technical problems.",
+//                                 '10-27-2012' => "We apologize but today's live stream is currently having technical problems.",
+
+                               );
+        date_default_timezone_set("America/New_York");
+        if((Date("NHi") >= 61030 && Date("NHi") <= 61300 && !array_key_exists(Date("n-j-Y"), $no_live_stream)) || $_GET['livetest'] == "golive" || isset($DB_Data[0])) {
+          If (mysql_ping()) {
             $temp = mysql_query("select title, page_id from article where title like 'Bulletin %'");
             While($tr = mysql_fetch_assoc($temp)) {
-              preg_match('/([0-9]{1,4})[_\\.\/-]([0-9]{1,2})[_\\.\/-]([0-9]{1,4})/i', $tr["title"], $matches);
+              preg_match('/([0-9]{1,4})[_./-]([0-9]{1,2})[_./-]([0-9]{1,4})/i', $tr["title"], $matches);
               If (isset($matches[1])) {
                 If (strlen($matches[1]) == 4) {
                   $bulletin_db[$matches[1]."-".($matches[2] - 0)."-".($matches[3] - 0)] = "/article/".$tr["page_id"];
@@ -60,36 +29,122 @@ If (mysql_ping()) {
                 }
               }
             }
-            if (isset($bulletin_db[date("Y-n-j", strtotime($podcast[0]["file_date"]))])) {
-              $temp = "<a href='".$bulletin_db[date("Y-n-j", strtotime($podcast[0]["file_date"]))]."' target = '_blank'>Bulletin</a>";
-            } else {
-              $temp = "";
+            $temp = mysql_query("select title, speaker, file_date, link from podcast where file_date = '".date("Y-m-d")."' and zone = 1 Order by status desc");
+            $tr = mysql_fetch_assoc($temp);
+          }
+          if (isset($DB_Data[0])) {
+            $tr["title"] = $DB_Data[0]["Title"];
+            $tr["speaker"] = $DB_Data[0]["ContactName"];
+          }
+          echo '<h3>Live Stream of Service</h3>';
+            If (isset($tr["title"])) {
+              Echo "<h4>".$tr["title"].(isset($tr["speaker"]) && trim($tr["speaker"]) != ""?" (".$tr["speaker"].")":"")."</h4>";
             }
+            if (isset($bulletin_db[date("Y-n-j")])) {
+            Echo "<p><a href='".$bulletin_db[date("Y-n-j")]."' target = '_blank'>Bulletin</a></p>";
+            }
+          echo '<iframe id="liveStream" src="http://stream.adventistuniversity.edu/SmoothStreamingPlayer.html" >&nbsp;  </iframe>';
+//          echo '<iframe id="liveStream" src="http://www.youtube.com/embed/yTpD7JEwTsA" frameborder="0" allowfullscreen></iframe>';
+//          echo '<iframe id="liveStream" src="http://streaming.priserv.com/SmoothStreamingPlayer.html" >&nbsp;  </iframe>';
+          echo "<script>";
+          echo "  _gaq.push(function() {";
+          echo "  var pageTracker = _gat.b._getTrackerByName();";
+          echo "  var iframe = document.getElementById('liveStream');";
+          echo "  iframe.src = b.pageTracker._getLinkerUrl('https://mars.adu.edu/SpringMeadowsSDA.html');
+});";
+          echo "</script>";
+          if (!isset($_GET['chattest']) || $_GET['chattest'] != "chat") {
+//echo "<div style='display:none'>";
+          }
+?><script id="sid0010000016015796269">(function() {function async_load(){s.id="cid0010000016015796269";s.src='http://st.chatango.com/js/gz/emb.js';s.style.cssText="width:350px;height:520px;";s.async=true;s.text='{"handle":"springmeadowschurch","styles":{"b":100,"f":50,"l":"999999","q":"999999","r":100,"s":1,"t":0,"w":0}}';var ss = document.getElementsByTagName('script');for (var i=0, l=ss.length; i < l; i++){if (ss[i].id=='sid0010000016015796269'){ss[i].id +='_';ss[i].parentNode.insertBefore(s, ss[i]);break;}}}var s=document.createElement('script');if (s.async==undefined){if (window.addEventListener) {addEventListener('load',async_load,false);}else if (window.attachEvent) {attachEvent('onload',async_load);}}else {async_load();}})();</script><?php
+          if (!isset($_GET['chattest']) || $_GET['chattest'] != "chat") {
+//echo "</div>";
+          }
+        }
+        else {
+      ?>
+<div id="superGraphic"><img alt="A Caring Church" src="/site/1/template/images/1_A_Caring_Church.jpg" /></div>
+<?php if (array_key_exists(Date("n-j-Y"), $no_live_stream)) { ?>
+
+<div>
+<p><?php echo $no_live_stream[Date("n-j-Y")]; ?></p>
+</div>
+<?php } ?><?php } ?>
+
+<div id="specialAnnounce">##custom4##</div>
+
+<div style="text-align: center;">
+<div class="third" style="margin-right: 0px; border-right-width: 0px;">
+<h3>Welcome&nbsp;</h3>
+
+<p>I am delighted that you are visiting our website. We want to share our love for Jesus with you and provide meaningful resources that will support your spiritual growth. We offer vibrant nurturing programs for seniors, couples, singles, youth, and children which include engaging activities during the week.</p>
+
+<p>We would love for you to visit us in our new facility and be touched by the Holy Spirit through our worship services which are:</p>
+
+<p style="text-align: left;"><strong>Saturdays</strong>:<br />
+<em>9:30 am</em> Bible study for adults, youth, and children<br />
+<em>10:45 am</em> Worship<br />
+<br />
+A live stream of the worship service is available on the top of this page every Sabbath (Saturday) morning starting at 10:45 am.</p>
+
+<p>I look forward to meeting each of you,</p>
+
+<p>With Christian love,</p>
+
+<p>Pastor Frank</p>
+
+<h3>Click for Directions</h3>
+
+<p id="directions"><?php 
+                    $ver = array();
+                    $dest = urlencode('5783 N Ronald Reagan Blvd. Sanford, FL 32773');
+                    preg_match("/.*CPU [^ ]*( ){0,1}OS ([0-9]+)_([0-9]+) like Mac OS.*/", $_SERVER['HTTP_USER_AGENT'], $ver);
+                    //Check for IOS Version 6 or above
+                    //If you are editing this be sure to edit the template as well.
+                    if(isset($ver[2]) && $ver[2] >= 6) {
+                      Echo '			<a href="http://maps.apple.com/maps?daddr='.$dest.'"><img src="/site/1/images/directions.jpg" /></a>';
+                    } 
+                    else {
+                      Echo '			<a href="http://maps.google.com/maps?saddr=&amp;daddr='.$dest.'"><img src="/site/1/images/directions.jpg" /></a>';
+                    }
+                  ?></p>
+</div>
+
+<div class="third" style="margin-right: 0px; border-right-width: 0px;">
+<h3>Thought For Today</h3>
+##rssdigest14##
+
+<h3>Latest Message</h3>
+<?php
+If (mysql_ping()) {
+  $temp = mysql_query("select title, speaker, file_date, link from podcast where zone = 1 and Status = 'active'  Order by file_date desc  LIMIT 1");
+  while (($tr = mysql_fetch_assoc($temp)) != false) {
+    $podcast[] = $tr;
+  }
 }
 if (count($podcast) > 0) {
- Echo "<a class='control_audio' href='".trim($podcast[0]["link"])."' >\n    <audio preload>\n      <source src='".trim($podcast[0]["link"])."' type='audio/mpeg' />\n    </audio>\n      <object id='flash_obj' class='playerpreview' type='application/x-shockwave-flash' data='/site/1/template/script/player_mp3_js.swf' width='0' height='0'>\n        <param name='movie' value='/site/1/template/script/player_mp3_js.swf' />\n        <param name='AllowScriptAccess' value='always' />\n        <param name='FlashVars' value='mp3=".trim($podcast[0]["link"])."' />\n      </object>\n      <img class='floatLeft third_thumbnail' src='/site/1/template/images/Play_Button.png' alt='Play Button' /></a><span class='sermon_title'><a href='".trim($podcast[0]["link"])."' >".str_replace("\\", "", $podcast[0]["title"])."</a></span><p class='tighter_text'>".str_replace("\\", "", $podcast[0]["speaker"])."<br /><br />".$temp."</p>";
+ Echo "<a class='control_audio' href='".trim($podcast[0]["link"])."' >\n    <audio preload>\n      <source src='".trim($podcast[0]["link"])."' type='audio/mpeg' />\n    </audio>\n      <object id='flash_obj' class='playerpreview' type='application/x-shockwave-flash' data='/site/1/template/script/player_mp3_js.swf' width='0' height='0'>\n        <param name='movie' value='/site/1/template/script/player_mp3_js.swf' />\n        <param name='AllowScriptAccess' value='always' />\n        <param name='FlashVars' value='mp3=".trim($podcast[0]["link"])."' />\n      </object>\n      <img class='floatLeft third_thumbnail' src='/site/1/template/images/Play_Button.png' alt='Play Button' /></a><span class='sermon_title'><a href='".trim($podcast[0]["link"])."' >".$podcast[0]["title"]."</a></span><p class='tighter_text'>".$podcast[0]["speaker"]."</p>";
 }
 If (false) {
 ?>
-		<audio preload="">
-			<a class="control_audio" href=""><source src="" type="audio/mpeg"></source></a></audio>
-		<object class="playerpreview" data="/site/1/template/script/player_mp3_js.swf" height="0" id="flash_obj" type="application/x-shockwave-flash" width="0"><source src="" type="audio/mpeg"><source src="" type="audio/mpeg"><source src="" type="audio/mpeg"><source src="" type="audio/mpeg"><source src="" type="audio/mpeg"><source src="" type="audio/mpeg"><source src="" type="audio/mpeg"><source src="" type="audio/mpeg"><param name="movie" value="/site/1/template/script/player_mp3_js.swf" />\n<param name="AllowScriptAccess" value="always" />\n<param name="FlashVars" value="mp3=" /></source></source></source></source></source></source></source></source></object><img alt="Play Button" class="floatLeft third_thumbnail" src="/site/1/template/images/Play_Button.png" /> <span class="sermon_title"><a href="">Auto-generated Sermon Title</a></span>
-		<p class="tighter_text">
-			Auto-generated Speaker Name</p>
-		<?php } ?>
-		<p>
-			<a href="http://www.springmeadows.org/article/92/rejoice/sermons">Past Sermons</a></p>
-		<h3>
-			<span style="font-family: georgia, serif;">Latest Blog Posts</span></h3>
-		<span style="font-family: georgia, serif;">##rssdigest15##</span>
-		<h3>
-			Connect</h3>
-		<div id="socialIcons">
-			<a href="http://www.facebook.com/pages/Spring-Meadows-Seventh-day-Adventist-Church/252734141407822"><img alt="Facebook Icon" src="/site/1/template/images/social/facebook.png" /></a> <a href="http://twitter.com/meadows_sda"><img alt="Twitter Icon" src="/site/1/template/images/social/twitter.png" /></a> <a href="http://youtube.com/wssdachurch"><img alt="Youtube Icon" src="/site/1/template/images/social/youtube.png" /></a> <a href="http://www.springmeadows.org/rss.php?zone=default"><img alt="RSS Icon" src="/site/1/template/images/social/rss.png" /></a> <a href="mailto:office@wssdachurch.com"><img alt="Email Icon" src="/site/1/template/images/social/email.png" /></a></div>
-	</div>
-	<div class="third" style="margin-right: 0px; border-right-width: 0px;">
-		<h3>
-			In the next few days...</h3>
-		##calendar## ##calendarlist##</div>
+
+<audio preload=""><source src="" type="audio/mpeg" /></audio>
+<object class="playerpreview" data="/site/1/template/script/player_mp3_js.swf" height="0" id="flash_obj" type="application/x-shockwave-flash" width="0"><source src="" type="audio/mpeg" /><source src="" type="audio/mpeg" /><source src="" type="audio/mpeg" /><source src="" type="audio/mpeg" /><source src="" type="audio/mpeg" /><source src="" type="audio/mpeg" /><source src="" type="audio/mpeg" /><source src="" type="audio/mpeg" /><source src="" type="audio/mpeg" /><source src="" type="audio/mpeg" /><source src="" type="audio/mpeg" /><source src="" type="audio/mpeg" /><source src="" type="audio/mpeg" /><param name="movie" value="/site/1/template/script/player_mp3_js.swf" />\n<param name="AllowScriptAccess" value="always" />\n<param name="FlashVars" value="mp3=" /></object><img alt="Play Button" class="floatLeft third_thumbnail" src="/site/1/template/images/Play_Button.png" /> <span class="sermon_title"><a href="">Auto-generated Sermon Title</a></span>
+
+<p class="tighter_text">Auto-generated Speaker Name</p>
+<?php } ?>
+
+<p><a href="http://www.springmeadows.org/article/92/rejoice/sermons">Past Sermons</a></p>
+
+<h3><span style="font-family:georgia,serif">Latest Blog Posts</span></h3>
+<span style="font-family:georgia,serif">##rssdigest15##</span></div>
+
+<div class="third" style="margin-right: 0px; border-right-width: 0px;">
+<h3>In the next few days...</h3>
+##calendar## ##calendarlist##
+
+<h3>Connect</h3>
+
+<div id="socialIcons"><a href="http://www.facebook.com/pages/Spring-Meadows-Seventh-day-Adventist-Church/252734141407822"><img alt="Facebook Icon" src="/site/1/template/images/social/facebook.png" /></a> <a href="http://twitter.com/meadows_sda"><img alt="Twitter Icon" src="/site/1/template/images/social/twitter.png" /></a> <a href="http://youtube.com/springmeadowssda"><img alt="Youtube Icon" src="/site/1/template/images/social/youtube.png" /></a> <a href="http://www.springmeadows.org/rss.php?zone=default"><img alt="RSS Icon" src="/site/1/template/images/social/rss.png" /></a> <a href="javascript:void(location.href='mailto:'+String.fromCharCode(111,102,102,105,99,101,64,115,112,114,105,110,103,109,101,97,100,111,119,115,46,111,114,103))"><img alt="Email Icon" src="/site/1/template/images/social/email.png" /></a></div>
 </div>
-<br />
+</div>
