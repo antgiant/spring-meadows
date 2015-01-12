9fbed5d751a083f81adcf02cafe370aeeac6babe
Travis Wooley <antgiant@gmail.com>
2015-01-12 13:24:45 -0500

    Removed filesystem check for podcast files. Made link correction far more robust against domain name changes. Removed unused html5 audio player that did not work.

--- Template/script/sermons.php
+++ Template/script/sermons.php
@@ -2,24 +2,9 @@
   if(!isset($selected_year)) {
     $selected_year = date("Y");
   }
-  $files = scandir("/home/wintersp/public_html/site/1/podcast/");
-  $dfiles = array();
   $bfiles = scandir("/home/wintersp/public_html/site/1/docs/");
   $bufiles = array();
   $years = array();
-  Foreach ($files as $file) {
-    preg_match('/([0-9]{2,4})[_\\.\/-]([0-9]{2})[_\\.\/-]([0-9]{2,4})/', $file, $matches);
-    If (isset($matches[1])) {
-      If (strlen($matches[1]) == 4) {
-        $dfiles[$matches[1]."-".($matches[2] - 0)."-".($matches[3] - 0)] = $file;
-        $years[$matches[1]] = $matches[1];
-      }
-      If (strlen($matches[3]) == 4) {
-        $dfiles[$matches[3]."-".($matches[1] - 0)."-".($matches[2] - 0)] = $file;
-        $years[$matches[3]] = $matches[3];
-      }
-    }
-  }
   Foreach ($bfiles as $file) {
     preg_match('/bulletin.*?([0-9]{1,4})[_\\.\/-]([0-9]{1,2})[_\\.\/-]([0-9]{1,4})/i', $file, $matches);
     If (isset($matches[1])) {
@@ -83,40 +68,21 @@
             }
             $output = "";
             $oldDay = $day;
-            $title = preg_replace("/^.*[0-9]{2,4}-[0-9]{2}-[0-9]{2,4} - (.*)\\.mp3$/","$1", str_replace("_", " ", $dfiles[$year.'-'.$i."-".$day]));
-            $preacher = preg_replace("/^.*?\((.*?)\).*/","$1", $title);
-            If ($title == $preacher) { $preacher = ""; }
-            $title = preg_replace("/^.*?\) */","", $title);
-            $title = preg_replace("/^\(/","", $title);
+            $title = "";
+            $preacher = "";
             If (!isset($podcast[$year.'-'.$i."-".$day]) && strlen(Trim($title)) < 3 && strlen(trim($preacher)) > 1) { $title = "Untitled"; }
             If (isset($podcast[$year.'-'.$i."-".$day]['title']) && strlen(Trim($podcast[$year.'-'.$i."-".$day]['title'])) < 3) { $podcast[$year.'-'.$i."-".$day]['title'] = "Untitled"; }
             If (isset($podcast[$year.'-'.$i."-".$day])) {
               $link = "";
-              If (strlen($podcast[$year.'-'.$i."-".$day]['link']) > 5 && $podcast[$year.'-'.$i."-".$day]['status'] == 'active' && trim($podcast[$year.'-'.$i."-".$day]['link']) != 'http://www.wintersprings22.adventistchurchconnect.org/site/1/podcast/' && trim($podcast[$year.'-'.$i."-".$day]['link']) != 'http://www.springmeadows.org/site/1/podcast/') {
-                $link = str_replace("http://www.wintersprings22.adventistchurchconnect.org", "", $podcast[$year.'-'.$i."-".$day]['link']);
+              If (strlen($podcast[$year.'-'.$i."-".$day]['link']) > 5 && $podcast[$year.'-'.$i."-".$day]['status'] == 'active' && preg_replace ('/(.*)(http[s]{0,1}:\/\/[^\/]*\/site\/1\/podcast\/)(.*)/ism', '$1$3', $podcast[$year.'-'.$i."-".$day]['link']) != '') {
+                $link = preg_replace('/(.*)(http[s]{0,1}:\/\/[^\/]*\/site\/1\/podcast\/)(.*)/ism', '$1/site/1/podcast/$3', $podcast[$year.'-'.$i."-".$day]['link']);
               }
               $title = str_replace("\\'", "'", str_replace('\\"', '"', $podcast[$year.'-'.$i."-".$day]['title']));
               if (trim($podcast[$year.'-'.$i."-".$day]['speaker']) != "")
                 $preacher = trim($podcast[$year.'-'.$i."-".$day]['speaker']);
             }
-            If (!isset($podcast[$year.'-'.$i."-".$day]) && isset($dfiles[$year.'-'.$i."-".$day])) {
-              $link = "site/1/podcast/".$dfiles[$year.'-'.$i."-".$day];
-            }
             if ($day != "" && $title != "") {
               $output .= "    <li value='".$day."'>\n";
-              if (FALSE && $link != "") {
-                $output .= "<a class='control_audio' href='".$link."' >\n";
-                $output .= "  <audio preload>\n";
-                $output .= "    <source src='".$link."' type='audio/mpeg' />\n";
-                $output .= "  </audio>\n";
-                $output .= "  <object id='flash_obj' class='playerpreview' type='application/x-shockwave-flash' data='/site/1/template/script/player_mp3_js.swf' width='0' height='0'>\n";
-                $output .= "    <param name='movie' value='/site/1/template/script/player_mp3_js.swf' />\n";
-                $output .= "    <param name='AllowScriptAccess' value='always' />\n";
-                $output .= "    <param name='FlashVars' value='mp3=".$link."' />\n";
-                $output .= "  </object>\n";
-                $output .= "  <img src='/site/1/template/images/Play_Button.png' alt='Play Button' />\n";
-                $output .= "</a>\n";
-              }
               $output .= ($link == ""?"":"<a href='".$link."'>");
               $output .= $title;
               $output .= ($link == ""?"":"</a>");
